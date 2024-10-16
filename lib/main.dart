b480b6ef6c0ee059e6aac7854de809190facd9e3
safwanpulisseri <safwanpulisseri123@gmail.com>
2024-10-16 10:54:52 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:e_commerce/home/view/page/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+     
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        useMaterial3: true,
+      ),
+      home:  HomePage(),
+    );
+  }
+}
